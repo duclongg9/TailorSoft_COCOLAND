067d15b14dd75862b336e544d52dbe4fb7184c89
duclongg9 <duclongg9@gmail.com>
2025-08-02 17:33:07 +0700

    .

--- TailorSoft_COCOLAND/src/java/model/ProductType.java
+++ TailorSoft_COCOLAND/src/java/model/ProductType.java
@@ -20,5 +20,26 @@ public class ProductType {
     public void setName(String name) { this.name = name; }
 
     public String getCode() { return code; }
-    public void setCode(String code) { this.code = code; }
+    
+    /**
+     * Sets the textual code identifying the product type.
+     *
+     * @param code the code value as stored in the database
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Overloaded setter accepting numeric codes. This helps avoid
+     * {@link NoSuchMethodError} when older compiled classes still
+     * reference a version of this method with an {@code int}
+     * parameter. The numeric value is converted to a string before
+     * assignment.
+     *
+     * @param code the numeric representation of the code
+     */
+    public void setCode(int code) {
+        this.code = String.valueOf(code);
+    }
 }
